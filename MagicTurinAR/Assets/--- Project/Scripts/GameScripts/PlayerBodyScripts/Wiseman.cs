547e36526fa18949dd531a65a24c69e492cdb2ec
Flavio Delpiano <flaviodelpiano@gmail.com>
2022-02-02 16:13:18 +0100

    interaction between players

--- MagicTurinAR/Assets/--- Project/Scripts/GameScripts/PlayerBodyScripts/Wiseman.cs
+++ MagicTurinAR/Assets/--- Project/Scripts/GameScripts/PlayerBodyScripts/Wiseman.cs
@@ -2,18 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System;
 
 public class Wiseman : MagicPlayer
 {
+    [SerializeField] private int whiteMagicToSend = 100;
     private void Update()
     {
-        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        //    RaycastHit hit;
+
+        //    //Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100f);
+
+        //    if (Physics.Raycast(ray, out hit))
+        //    {
+        //        Debug.Log(hit.transform.name);
+        //        if (hit.collider != null)
+        //        {
+
+        //            GameObject touchedObject = hit.transform.gameObject;
+
+        //            Debug.Log("Touched " + touchedObject.transform.name);
+        //        }
+        //    }
+        //}
+
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            
-            //Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100f);
-           
+
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.transform.name);
@@ -22,16 +42,36 @@ public class Wiseman : MagicPlayer
 
                     GameObject touchedObject = hit.transform.gameObject;
 
-                    Debug.Log("Touched " + touchedObject.transform.name);
+                    if (touchedObject.tag == "Player")
+                    {
+                        SendWhiteMagic(touchedObject, whiteMagicToSend);
+                        uiInventory.textGems.text = (Int32.Parse(uiInventory.textGems.text) - 1).ToString();
+                        foreach (MagicItemSO item in ItemAssets.Instance.magicInventorySO.items)
+                        {
+                            if (item.id == 2000) // White Fragment specific code
+                            {
+                                item.prefab.GetComponent<MagicItem>().amount--;
+                            }
+                        
+                        }
+                    }
                 }
             }
+
         }
     }
     [Command]
     public void SendWhiteMagic(GameObject target, int whiteMagicToSend)
-    { 
-
-
+    {
+        Debug.Log("Wiseman manda magia bianca");
+        NetworkIdentity opponentIdentity = target.GetComponent<NetworkIdentity>();
+        TargetReceiveWhiteMagic(opponentIdentity.connectionToClient, whiteMagicToSend);
     }
-    
+
+
+    [TargetRpc]
+    public void TargetReceiveWhiteMagic(NetworkConnection target, int whiteMagicReceived)
+    {
+        Debug.Log("Player receives white magic:" + whiteMagicReceived);
+    }
 }
