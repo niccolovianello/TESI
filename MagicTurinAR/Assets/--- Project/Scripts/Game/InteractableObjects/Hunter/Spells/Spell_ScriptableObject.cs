5f8875a44b568c255f2ef5f31582f69e462240d8
Niccolò Vianello <niccolo.vianello@studenti.polito.it>
2022-02-11 18:20:58 +0100

    cardsSO

--- MagicTurinAR/Assets/--- Project/Scripts/Game/InteractableObjects/Hunter/Spells/Spell_ScriptableObject.cs
+++ MagicTurinAR/Assets/--- Project/Scripts/Game/InteractableObjects/Hunter/Spells/Spell_ScriptableObject.cs
@@ -1,15 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Spell", menuName = "Spells")]
-
 public class Spell_ScriptableObject : ScriptableObject
 {
-    public float Cost = 5f;
-    public float Lifetime = 2f;
-    public float Damage = 10f;
-    public float Radius = .5f;
+    public float Cost;
+    public float Lifetime;
+    public float Damage;
+    public float Radius;
     
     public const float MAX_FORCE = 2000f;
 
