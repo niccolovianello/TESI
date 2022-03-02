6adbf3aa7564a61f605d83a5e24c21d9b3650e1e
Flavio Delpiano <flaviodelpiano@gmail.com>
2022-03-02 10:42:37 +0100

    timeout push + loc provider

--- MagicTurinAR/Assets/--- Project/Scripts/Utility/GameManager.cs
+++ MagicTurinAR/Assets/--- Project/Scripts/Utility/GameManager.cs
@@ -100,7 +100,7 @@ public class GameManager : Singleton<GameManager>
         networkPlayerCamera.enabled = true;
         audioListener.enabled = true;
         
-        //networkPlayer.gameObject.AddComponent<ImmediatePositionWithLocationProvider>();
+        networkPlayer.gameObject.AddComponent<ImmediatePositionWithLocationProvider>();
         networkPlayer.gameObject.AddComponent<RotateWithLocationProvider>();
         
 
