86669e19c647582ef76836f5c520f3dce57d4355
Flavio Delpiano <flaviodelpiano@gmail.com>
2022-04-06 12:09:56 +0200

    camera position log push

--- MagicTurinAR/Assets/--- Project/Scripts/Game/UIGame/UI_EnemyFight.cs
+++ MagicTurinAR/Assets/--- Project/Scripts/Game/UIGame/UI_EnemyFight.cs
@@ -8,10 +8,17 @@ using NetworkPlayer = MirrorBasics.NetworkPlayer;
 public class UI_EnemyFight : MonoBehaviour
 {
     public Canvas canvasWindowPlayerDefeat;
+    public Text cameraPosition;
+    public Camera camera3;
 
     public void PlayerDefeatedByDemon()
     {
         canvasWindowPlayerDefeat.enabled = true;
+        //camera3 = FindObjectOfType<Camera>();
+    }
+    private void Update()
+    {
+        cameraPosition.text = "x: " + camera3.transform.position.x + "\nz: " + camera3.transform.position.z;
     }
 
     public void RestartFight()
