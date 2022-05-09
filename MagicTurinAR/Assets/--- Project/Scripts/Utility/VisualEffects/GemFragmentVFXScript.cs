239266982712a7ab9ae09a160ce06d26f411b180
Flavio Delpiano <flaviodelpiano@gmail.com>
2022-05-09 15:37:34 +0200

    vfx fix

---
+++ MagicTurinAR/Assets/--- Project/Scripts/Utility/VisualEffects/GemFragmentVFXScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemFragmentVFXScript : MonoBehaviour
+{
+    [SerializeField]
+    private float velocity = 20f;
+    [SerializeField]
+    private Vector3 offsetMidPoint = new Vector3(0, 9, 0);
+    [SerializeField]
+    private Vector3 offsetStartEndPoint = new Vector3(0, 3, 0);
+
+    private Vector3 startMarker, endMarker;
+    private Vector3[] waypoints;
+    private int currentStartPoint;
+    private float startTime;
+    private float journeyLength;
+
+    void Start()
+    {
+        currentStartPoint = 0;
+
+        SetPoints();
+
+    }
+    public IEnumerator TransitionPrtycleSystemCoroutine(GameObject go, GameObject netplay1, GameObject netplay2)
+    {
+        Vector3 midPoint = FindMidPoint(netplay1.gameObject.transform.position, netplay2.gameObject.transform.position);
+        
+        waypoints = new Vector3[] { netplay1.gameObject.transform.position + offsetStartEndPoint, FindMidPoint(netplay1.gameObject.transform.position, netplay2.gameObject.transform.position) + offsetMidPoint, netplay2.gameObject.transform.position + offsetStartEndPoint};
+
+        SetPoints();
+
+        float distCovered = (Time.time - startTime);
+        float fracJourney = distCovered / journeyLength;
+
+        
+        while (fracJourney < 1f || currentStartPoint + 2 <= waypoints.Length)
+        {
+            distCovered = (Time.time - startTime) * velocity;
+            fracJourney = distCovered / journeyLength;
+            go.transform.position = Vector3.Slerp(startMarker, endMarker, fracJourney);
+
+
+            if (fracJourney >= 1f && currentStartPoint + 2 < waypoints.Length)
+            {
+                currentStartPoint++;
+                SetPoints();
+                fracJourney = 0;
+
+            }
+            if (fracJourney >= 1f && currentStartPoint + 2 == waypoints.Length)
+            {
+
+                currentStartPoint++;
+            }
+
+
+            yield return null;
+        }
+
+        Debug.Log("uscito");
+        currentStartPoint = 0;
+        Destroy(go);
+
+    }
+
+    private void SetPoints()
+    {
+        startMarker = waypoints[currentStartPoint];
+        endMarker = waypoints[currentStartPoint + 1];
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+        startTime = Time.time;
+
+    }
+    static private Vector3 FindMidPoint(Vector3 starpos, Vector3 endpos)
+    {
+        return Vector3.Lerp(starpos, endpos, 0.5f);
+    }
+}
--- MagicTurinAR/Assets/--- Project/Scripts/Utility/VisualEffects/GraphicInterctionBetweenPlayers.cs
+++ MagicTurinAR/Assets/--- Project/Scripts/Utility/VisualEffects/GraphicInterctionBetweenPlayers.cs
@@ -8,16 +8,15 @@ public class GraphicInterctionBetweenPlayers : MonoBehaviour
 {
     public GameObject prefabWhiteMagicParticleSystem;
     public GameObject prefabGemParticleSystem;
-    public Vector3 offset = new Vector3(0, 2, 0);
-    public Vector3 offsetMid = new Vector3(0, 7, 0);
-    public float scaleMultiplier = 8;
+
+    public float scaleMultiplier = 10;
 
 
     public void WisemanSendWhiteMagic( GameObject netplay1, GameObject netplay2)
     {
         GameObject go = Instantiate(prefabWhiteMagicParticleSystem, netplay1.gameObject.transform.position, Quaternion.identity);
         go.transform.localScale *= scaleMultiplier;
-        StartCoroutine(TransitionPrtycleSystemCoroutine(go, netplay1, netplay2));
+        StartCoroutine(go.GetComponent<GemFragmentVFXScript>().TransitionPrtycleSystemCoroutine(go, netplay1, netplay2));
         Debug.Log("Visual effetc white magic: player1" + netplay1 + "    player2" + netplay2);
         
 
@@ -27,42 +26,8 @@ public class GraphicInterctionBetweenPlayers : MonoBehaviour
     {
         GameObject go = Instantiate(prefabGemParticleSystem, netplay1.gameObject.transform.position, Quaternion.identity);
         go.transform.localScale *= scaleMultiplier;
-        StartCoroutine(TransitionPrtycleSystemCoroutine(go, netplay1, netplay2));
+        StartCoroutine(go.GetComponent<GemFragmentVFXScript>().TransitionPrtycleSystemCoroutine(go, netplay1, netplay2));
         Debug.Log("Visual effetc gem: player1" + netplay1 + "    player2" + netplay2);
     }
 
-    public IEnumerator TransitionPrtycleSystemCoroutine(GameObject go, GameObject netplay1, GameObject netplay2)
-    {
-
-        float elapsedTime = 0;
-        float waitTime = 2f;
-        while (elapsedTime < waitTime/2)
-        {
-            go.transform.position = Vector3.Slerp(netplay1.gameObject.transform.position + offset, FindMidPoint(netplay1.transform.position, netplay2.transform.position) + offsetMid, elapsedTime);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-            // Yield here
-
-        }
-        while (elapsedTime < waitTime)
-        {
-            go.transform.position = Vector3.Slerp(FindMidPoint(netplay1.transform.position, netplay2.transform.position) + offsetMid, netplay2.gameObject.transform.position + offset, elapsedTime);
-            elapsedTime += Time.deltaTime;
-
-            // Yield here
-            yield return null;
-        }
-        Destroy(go);
-        Vibration.VibratePeek();
-    }
-
-    private Vector3 FindMidPoint(Vector3 starpos, Vector3 endpos)
-    {
-        Vector3 result = new Vector3();
-        result.x = starpos.x + (endpos.x - starpos.x / 2);
-        result.y = starpos.y + (endpos.y - starpos.y / 2);
-        result.z = starpos.z + (endpos.z - starpos.z / 2);
-
-        return result;
-    }
 }
